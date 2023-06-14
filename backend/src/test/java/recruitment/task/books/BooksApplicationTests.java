b71c91749724c2a5da5c700a9466f806ea30a2e6
RoZ0o0 <62017852+RoZ0o0@users.noreply.github.com>
2023-06-14 15:57:50 +0200

    Setting up the project with React and SpringBoot

---
+++ backend/src/test/java/recruitment/task/books/BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package recruitment.task.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
